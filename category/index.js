cc2ac5ee06073ab2ffc009054286ef02c1cb3f9c
Tesfaye Hailu <thailu@LABA.PSWAS.LOCAL>
2023-07-10 17:27:43 -0700

    categories

---
+++ category/index.js
@@ -0,0 +1,13 @@
+export const categories = [
+    {name: 'Data Science ', sortOrder: 10},
+    {name: 'Web Development', sortOrder: 20},
+    {name: 'Network Administration', sortOrder: 30},
+    {name: 'Cloud Computing', sortOrder: 40},
+    {name: 'Database Manaement', sortOrder: 50},
+    {name: 'Software Engineering', sortOrder: 60},
+    {name: 'Cyber Security', sortOrder: 70},
+    {name: 'IT Support', sortOrder: 80},
+    {name: 'Machine learning', sortOrder: 90},
+    {name: 'Artificial Intelligence', sortOrder: 100}
+  ]
+  
